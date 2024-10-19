b4f41b03c811ac73a62184343b1afc5fd45e4bdd
eyaadh <eyaadh@gmail.com>
2024-10-19 13:35:23 +0500

    enhancements: - moved top nav menu to its own component as its shared across views.

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -14,136 +14,37 @@
             </p>
           </div>
 
-          <div class="flex items-center justify-between">
-            <Menu as="div" class="relative ml-3 inline-block text-left">
-              <div>
-                <MenuButton
-                  class="-my-2 flex items-center rounded-full bg-white/5 p-2 text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          <ThemeNavMenu>
+            <template #pageNavs>
+              <MenuItem v-slot="{ active }">
+                <button
+                  @click="openProductDialog"
+                  type="button"
+                  :class="[
+                    active ? 'bg-gray-100/80 text-gray-900' : 'text-white',
+                    'flex w-full items-center justify-start gap-2 px-4 py-2 text-sm',
+                  ]"
                 >
-                  <span class="sr-only">Open options</span>
-                  <EllipsisVerticalIcon class="h-5 w-5" aria-hidden="true" />
-                </MenuButton>
-              </div>
+                  <PlusCircleIcon class="mb-1 h-4 w-4" aria-hidden="true" />
+                  <span class="drop-shadow">Add Products</span>
+                </button>
+              </MenuItem>
 
-              <transition
-                enter-active-class="transition ease-out duration-100"
-                enter-from-class="transform opacity-0 scale-95"
-                enter-to-class="transform opacity-100 scale-100"
-                leave-active-class="transition ease-in duration-75"
-                leave-from-class="transform opacity-100 scale-100"
-                leave-to-class="transform opacity-0 scale-95"
-              >
-                <MenuItems
-                  class="absolute right-2 z-50 mt-4 w-56 origin-top-right rounded-md bg-gray-900/55 shadow-lg ring-2 ring-white/10 ring-opacity-5 backdrop-blur-sm focus:outline-none"
+              <MenuItem v-slot="{ active }">
+                <button
+                  @click="openCategoriesDialog"
+                  type="button"
+                  :class="[
+                    active ? 'bg-gray-100/80 text-gray-900' : 'text-white',
+                    'flex w-full items-center justify-start gap-2 border-b border-white/10 px-4 py-2 text-sm',
+                  ]"
                 >
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <button
-                        @click="openProductDialog"
-                        type="button"
-                        :class="[
-                          active
-                            ? 'bg-gray-100/80 text-gray-900'
-                            : 'text-white',
-                          'flex w-full items-center justify-start gap-2 border-b border-white/10 px-4 py-2 text-sm',
-                        ]"
-                      >
-                        <PlusCircleIcon
-                          class="mb-1 h-4 w-4"
-                          aria-hidden="true"
-                        />
-                        <span class="drop-shadow">Add Products</span>
-                      </button>
-                    </MenuItem>
-
-                    <MenuItem v-slot="{ active }">
-                      <button
-                        @click="openCategoriesDialog"
-                        type="button"
-                        :class="[
-                          active
-                            ? 'bg-gray-100/80 text-gray-900'
-                            : 'text-white',
-                          'flex w-full items-center justify-start gap-2 border-b border-white/10 px-4 py-2 text-sm',
-                        ]"
-                      >
-                        <TagIcon class="mb-1 h-4 w-4" aria-hidden="true" />
-                        <span class="drop-shadow">Manage Categories</span>
-                      </button>
-                    </MenuItem>
-
-                    <MenuItem v-slot="{ active }">
-                      <RouterLink
-                        to="/reviewManagement"
-                        :class="[
-                          active
-                            ? 'bg-gray-100/80 text-gray-900'
-                            : 'text-white',
-                          'flex w-full items-center justify-start gap-2 border-b border-white/10 px-4 py-2 text-sm',
-                        ]"
-                      >
-                        <ChatBubbleBottomCenterTextIcon
-                          class="mb-1 h-4 w-4"
-                          aria-hidden="true"
-                        />
-                        <span class="drop-shadow">Review Management</span>
-                      </RouterLink>
-                    </MenuItem>
-
-                    <MenuItem v-slot="{ active }">
-                      <RouterLink
-                        to="/"
-                        :class="[
-                          active
-                            ? 'bg-gray-100/80 text-gray-900'
-                            : 'text-white',
-                          'flex w-full items-center justify-start gap-2 px-4 py-2 text-sm',
-                        ]"
-                      >
-                        <HomeIcon class="mb-1 h-4 w-4" aria-hidden="true" />
-                        <span class="drop-shadow">Customer View</span>
-                      </RouterLink>
-                    </MenuItem>
-
-                    <MenuItem v-slot="{ active }">
-                      <RouterLink
-                        to="/qr"
-                        type="button"
-                        :class="[
-                          active
-                            ? 'bg-gray-100/80 text-gray-900'
-                            : 'text-white',
-                          'flex w-full items-center justify-start gap-2 border-b border-white/10 px-4 py-2 text-sm',
-                        ]"
-                      >
-                        <QrCodeIcon class="mb-1 h-4 w-4" aria-hidden="true" />
-                        <span class="drop-shadow">Show my QR</span>
-                      </RouterLink>
-                    </MenuItem>
-
-                    <MenuItem v-slot="{ active }">
-                      <button
-                        type="button"
-                        @click="signOutUser"
-                        :class="[
-                          active
-                            ? 'bg-gray-100/80 text-gray-900'
-                            : 'text-white',
-                          'flex w-full items-center justify-start gap-2 px-4 py-2 text-sm',
-                        ]"
-                      >
-                        <ArrowLeftStartOnRectangleIcon
-                          class="mb-1 h-4 w-4"
-                          aria-hidden="true"
-                        />
-                        <span class="drop-shadow">Sign Out</span>
-                      </button>
-                    </MenuItem>
-                  </div>
-                </MenuItems>
-              </transition>
-            </Menu>
-          </div>
+                  <TagIcon class="mb-1 h-4 w-4" aria-hidden="true" />
+                  <span class="drop-shadow">Manage Categories</span>
+                </button>
+              </MenuItem>
+            </template>
+          </ThemeNavMenu>
         </div>
       </div>
       <div class="h-full w-full overflow-auto [&::-webkit-scrollbar]:hidden">
@@ -445,22 +346,16 @@
 </template>
 
 <script setup lang="ts">
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+import { MenuItem } from "@headlessui/vue";
 import {
   PlusCircleIcon,
-  QrCodeIcon,
-  HomeIcon,
-  EllipsisVerticalIcon,
   PhotoIcon,
-  ArrowLeftStartOnRectangleIcon,
   TagIcon,
   TrashIcon,
   CheckCircleIcon,
   ExclamationTriangleIcon,
   HashtagIcon,
-  ChatBubbleBottomCenterTextIcon,
 } from "@heroicons/vue/20/solid";
-import { useAuth } from "@/composables/useAuth";
 import ThemeDialog from "@/components/ThemeDialog.vue";
 import { computed, ref } from "vue";
 import { required } from "@vuelidate/validators";
@@ -472,8 +367,8 @@ import IconMVRF from "@/components/icons/IconMVRF.vue";
 import type { ICategory } from "@/types/Categories";
 import { useCategoriesStore } from "@/stores/useCategoriesStore";
 import ThemeCombobox from "@/components/ThemeCombobox.vue";
+import ThemeNavMenu from "@/components/ThemeNavMenu.vue";
 
-const { signOutUser } = useAuth();
 const productsDialog = ref<typeof ThemeDialog | null>(null);
 const categoriesDialog = ref<typeof ThemeDialog | null>(null);
 const productsStore = useProductsStore();
--- src/views/ReviewManagementView.vue
+++ src/views/ReviewManagementView.vue
@@ -14,101 +14,7 @@
             </p>
           </div>
 
-          <div class="flex items-center justify-between">
-            <Menu as="div" class="relative ml-3 inline-block text-left">
-              <div>
-                <MenuButton
-                  class="-my-2 flex items-center rounded-full bg-white/5 p-2 text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                >
-                  <span class="sr-only">Open options</span>
-                  <EllipsisVerticalIcon class="h-5 w-5" aria-hidden="true" />
-                </MenuButton>
-              </div>
-
-              <transition
-                enter-active-class="transition ease-out duration-100"
-                enter-from-class="transform opacity-0 scale-95"
-                enter-to-class="transform opacity-100 scale-100"
-                leave-active-class="transition ease-in duration-75"
-                leave-from-class="transform opacity-100 scale-100"
-                leave-to-class="transform opacity-0 scale-95"
-              >
-                <MenuItems
-                  class="absolute right-2 z-50 mt-4 w-56 origin-top-right rounded-md bg-gray-900/55 shadow-lg ring-2 ring-white/10 ring-opacity-5 backdrop-blur-sm focus:outline-none"
-                >
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <RouterLink
-                        to="/products"
-                        :class="[
-                          active
-                            ? 'bg-gray-100/80 text-gray-900'
-                            : 'text-white',
-                          'flex w-full items-center justify-start gap-2 border-b border-white/10 px-4 py-2 text-sm',
-                        ]"
-                      >
-                        <ShoppingCartIcon
-                          class="mb-1 h-4 w-4"
-                          aria-hidden="true"
-                        />
-                        <span class="drop-shadow">Products Management</span>
-                      </RouterLink>
-                    </MenuItem>
-
-                    <MenuItem v-slot="{ active }">
-                      <RouterLink
-                        to="/"
-                        :class="[
-                          active
-                            ? 'bg-gray-100/80 text-gray-900'
-                            : 'text-white',
-                          'flex w-full items-center justify-start gap-2 px-4 py-2 text-sm',
-                        ]"
-                      >
-                        <HomeIcon class="mb-1 h-4 w-4" aria-hidden="true" />
-                        <span class="drop-shadow">Customer View</span>
-                      </RouterLink>
-                    </MenuItem>
-
-                    <MenuItem v-slot="{ active }">
-                      <RouterLink
-                        to="/qr"
-                        type="button"
-                        :class="[
-                          active
-                            ? 'bg-gray-100/80 text-gray-900'
-                            : 'text-white',
-                          'flex w-full items-center justify-start gap-2 border-b border-white/10 px-4 py-2 text-sm',
-                        ]"
-                      >
-                        <QrCodeIcon class="mb-1 h-4 w-4" aria-hidden="true" />
-                        <span class="drop-shadow">Show my QR</span>
-                      </RouterLink>
-                    </MenuItem>
-
-                    <MenuItem v-slot="{ active }">
-                      <button
-                        type="button"
-                        @click="signOutUser"
-                        :class="[
-                          active
-                            ? 'bg-gray-100/80 text-gray-900'
-                            : 'text-white',
-                          'flex w-full items-center justify-start gap-2 px-4 py-2 text-sm',
-                        ]"
-                      >
-                        <ArrowLeftStartOnRectangleIcon
-                          class="mb-1 h-4 w-4"
-                          aria-hidden="true"
-                        />
-                        <span class="drop-shadow">Sign Out</span>
-                      </button>
-                    </MenuItem>
-                  </div>
-                </MenuItems>
-              </transition>
-            </Menu>
-          </div>
+          <ThemeNavMenu />
         </div>
       </div>
 
@@ -200,22 +106,11 @@
 </template>
 
 <script setup lang="ts">
-import {
-  ArrowLeftStartOnRectangleIcon,
-  EllipsisVerticalIcon,
-  HomeIcon,
-  QrCodeIcon,
-  ShoppingCartIcon,
-  PhoneIcon,
-  EnvelopeIcon,
-  StarIcon,
-} from "@heroicons/vue/20/solid";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
-import { useAuth } from "@/composables/useAuth";
+import { PhoneIcon, EnvelopeIcon, StarIcon } from "@heroicons/vue/20/solid";
 import { useReviewsStore } from "@/stores/useReviewsStore";
 import type { IReview } from "@/types/Reviews";
+import ThemeNavMenu from "@/components/ThemeNavMenu.vue";
 
-const { signOutUser } = useAuth();
 const reviewStore = useReviewsStore();
 
 async function approveReview(review: IReview) {
